4d3d3e3ab2edc29459fb2e5bad3c5f24700d5e74
Sriram Radhakrishna <83068519+SR42-dev@users.noreply.github.com>
2021-11-22 23:48:47 +0530

    Add files via upload

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sr42/robofest2021/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/sr42/robofest2021/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/sr42/robofest2021/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/sr42/robofest2021/src:$ROS_PACKAGE_PATH"
